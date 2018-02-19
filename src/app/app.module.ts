9ab2cad8fc56df9056d5bc3d5274cbe28a98fb25
joao.oliveira <joao.oliveira@maisvida.med.br>
2018-02-19 09:32:14 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule, Http } from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -14,6 +15,7 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
+    HttpModule,
     IonicModule.forRoot(MyApp)
   ],
   bootstrap: [IonicApp],
@@ -24,6 +26,7 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
+    HttpModule,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Geolocation } from '@ionic-native/geolocation';
+import { Http } from '@angular/http';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers:[Geolocation]
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private geo: Geolocation, private http: Http) {
+    this.geo.watchPosition()
+    .subscribe(
+      res => this.notify(res.coords)
+    )
+  }
 
+  public notify(coords){
+    console.log(coords)
+    let data = {
+      "lat": coords.latitude,
+      "long": coords.longitude
+    }
+
+    this.http.post("https://limitless-anchorage-11992.herokuapp.com/api/notify-users", data)
+    .subscribe(
+      res=> console.log(res),
+      err=>console.log(err)
+    )
   }
 
 }
